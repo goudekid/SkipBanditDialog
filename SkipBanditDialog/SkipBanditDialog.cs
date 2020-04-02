7dbf86de086e1ea562b84e63e911a2824f2b9ac7
Garrett Cox <goudekid@hotmail.com>
2020-04-01 23:57:42 -0700

    added git repo

---
+++ SkipBanditDialog/SkipBanditDialog.cs
@@ -0,0 +1,15 @@
+﻿using TaleWorlds.MountAndBlade;
+using HarmonyLib;
+
+namespace SkipBanditDialog
+{
+    internal class SkipBanditDialog : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            var harmony = new Harmony("com.goog.bannerlordmods.skipbanditdialog");
+            harmony.PatchAll();
+            base.OnSubModuleLoad();
+        }
+    }
+}
---
+++ SkipBanditDialog/SkipBanditDialogPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+
+namespace SkipBanditDialog
+{
+    [HarmonyPatch(typeof(ConversationManager), "SetupAndStartMapConversation")]
+    public class SkipBanditDialogPatch
+    {
+        [HarmonyPrefix]
+        static bool SetupDialogPostfix(ConversationManager __instance, MobileParty party, IAgent agent, IAgent mainAgent)
+        {
+            if (party.IsBandit && !party.IsLordParty && !party.IsBanditBossParty && !party.IsCurrentlyUsedByAQuest)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
